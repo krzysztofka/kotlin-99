e5d9cbc06b595b98f03a7906177034c3eaa56b93
kalisiakk <kalisiakk@gmail.com>
2019-07-25 23:59:05 +0200

    56 is symmetric

--- src/test/kotlin/com/kali/BinaryTree.kt
+++ src/test/kotlin/com/kali/BinaryTree.kt
@@ -1,5 +1,79 @@
 package com.kali
 
-open class BinaryTreeNode<T> (var value: T, var left: BinaryTreeNode<T>?, var right: BinaryTreeNode<T>?)
-//
-//class Tree<T> (root: BinaryTreeNode<T>)
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+sealed class Tree<out T> {
+    companion object {
+
+        @JvmStatic
+        fun <K> cBalanced(numberOfNodes: Int, value: K): List<Tree<K>> =
+                when {
+                    numberOfNodes < 1 -> listOf(End)
+                    numberOfNodes % 2 == 1 -> {
+                        val subtree = cBalanced(numberOfNodes / 2, value)
+                        subtree.flatMap { l -> subtree.map { r -> BinaryTreeNode(value, l, r) } }
+                    }
+                    else -> {
+                        val oddSubtree = cBalanced((numberOfNodes - 1) / 2 + 1, value)
+                        val evenSubtree = cBalanced((numberOfNodes - 1) / 2, value)
+                        oddSubtree.flatMap { o ->
+                            evenSubtree.flatMap { e -> listOf(BinaryTreeNode(value, o, e), BinaryTreeNode(value, e, o)) }
+                        }
+                    }
+                }
+    }
+
+    open fun isSymmetric(): Boolean = false
+
+    open fun isMirrorOf(t: Tree<Any?>): Boolean = false
+
+    object End : Tree<Nothing>() {
+        override fun isMirrorOf(t: Tree<Any?>): Boolean = t == End
+        override fun isSymmetric(): Boolean = true
+        override fun toString() = "."
+    }
+}
+
+data class BinaryTreeNode<out T>(
+        val value: T,
+        val left: Tree<T> = End,
+        val right: Tree<T> = End
+) : Tree<T>() {
+
+    override fun isSymmetric(): Boolean = left.isMirrorOf(right)
+
+    override fun isMirrorOf(t: Tree<Any?>): Boolean =
+            if (t is BinaryTreeNode) left.isMirrorOf(t.right) && right.isMirrorOf(t.left)
+            else false
+
+    override fun toString(): String {
+        return "T($value $left $right)"
+    }
+}
+
+class BinaryTreeTest {
+
+    @Test
+    fun `55 test c balanced`() {
+        assertThat(Tree.cBalanced(4, "x").toString())
+                .isEqualTo("[T(x T(x T(x . .) .) T(x . .)), T(x T(x . .) T(x T(x . .) .)), T(x T(x . T(x . .)) T(x . .)), T(x T(x . .) T(x . T(x . .)))]")
+    }
+
+    @Test
+    fun `56 test symmetric`() {
+        assertThat(BinaryTreeNode('a', BinaryTreeNode('b'), BinaryTreeNode('c')).isSymmetric())
+                .isTrue()
+
+        assertThat(BinaryTreeNode('a',
+                BinaryTreeNode('b', BinaryTreeNode('d'), Tree.End),
+                BinaryTreeNode('c')).isSymmetric())
+                .isFalse()
+
+        assertThat(BinaryTreeNode('a',
+                BinaryTreeNode('b', BinaryTreeNode('d'), Tree.End),
+                BinaryTreeNode('c', Tree.End, BinaryTreeNode('f')))
+                .isSymmetric())
+                .isTrue()
+    }
+}
